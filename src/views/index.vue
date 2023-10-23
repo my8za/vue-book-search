d25bdbfd0061d5386ea8bafa3fbfdab729669fc6
my8za <emilie15@naver.com>
2023-10-23 13:51:34 +0900

    pure css image-slide animation

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,9 +1,40 @@
 <template>
-  <div>home</div>
+  <div>
+    <h1>home</h1>
+    <div class="slide-container">
+      <img v-for="(img, idx) in imgUrls" :key="idx" :src="img" alt="banner" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      imgUrls: [
+        "https://image.aladin.co.kr/img/bn/book/2023/04/231005_nobel_layer.jpg",
+        "https://image.aladin.co.kr/img/bn/book/2023/04/231005_nobel_layer.jpg",
+        "https://image.aladin.co.kr/img/bn/book/2023/04/231005_nobel_layer.jpg",
+      ],
+    };
+  },
+};
 </script>
 
-<style></style>
+<style>
+@keyframes slide-ani {
+  to {
+    transform: translateX(-100%);
+  }
+}
+.slide-container {
+  display: flex;
+  width: 1028px;
+  height: 420px;
+  overflow: hidden;
+  /* animation: slide-ani 1s infinite; */
+}
+.slide-container > img {
+  animation: slide-ani 4s infinite;
+}
+</style>
